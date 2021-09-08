1d6153e3f0c7a29a4a0b8792838cb84253b467f9
StanciuMSebastian <34349665+StanciuMSebastian@users.noreply.github.com>
2021-09-08 15:05:30 +0300

    Delete Server directory

--- Server/CException.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "IException.h"
-class CException :
-    public IException
-{
-};
-
--- Server/CInode.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "INode.h"
-class CInode :
-    public INode
-{
-};
-
