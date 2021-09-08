552508f2af148c46284e35a7e096e2600fd13106
StanciuMSebastian <34349665+StanciuMSebastian@users.noreply.github.com>
2021-09-08 15:10:04 +0300

    Delete User.cpp

--- User.cpp
+++
@@ -1,6 +0,0 @@
-#include "User.h"
-User::User(std::string username, std::string password , std::string rank, int ID)
-{
-	this->username = username; this->password = password;
-	this->rank = rank; this->ID = ID;
-}
